847e85cd73ac35970bab138fb58b337baf964599
David Pekar <davidpekar@utexas.edu>
2023-09-26 21:38:45 -0500

    project-setup

---
+++ my-app/src/Pages/Login.tsx
@@ -0,0 +1,14 @@
+import { Container } from "@mui/material";
+
+
+function LoginPage(){
+    return (
+        <Container>
+            <h1>Cry</h1>
+        </Container>
+
+    );
+};
+
+
+export default LoginPage;
