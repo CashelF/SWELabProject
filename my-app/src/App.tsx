49c6535e3ce301cb63c3e54d56a6becbae8b2da0
David Pekar <davidpekar@utexas.edu>
2023-09-27 12:28:46 -0500

    initial_login_page

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,10 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 import LoginPage from './Pages/Login';
 
+
 function App() {
   return (
-    <div className="App">
-      <LoginPage/>
+    <div style={{margin: 0, padding: 0}}>
       <LoginPage/>
     </div>
   );
