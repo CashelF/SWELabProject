b84c438e650450a62fb3ff9b87ba67d25983f160
David Pekar <davidpekar@utexas.edu>
2023-10-05 18:41:05 -0500

    added-hero-section

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,11 +4,12 @@ import './App.css';
 import LoginPage from './Pages/LoginPage';
 import SignUpPage from './Pages/SignUpPage'
 import { Login } from '@mui/icons-material';
+import Hero from './Components/Hero';
 
 function App() {
   return (
     <div style={{margin: 0, padding: 0}}>
-      <LoginPage/>
+      <Hero/>
     </div>
   );
 }
---
+++ my-app/src/Components/HowItWorks.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Box, Button, styled, Typography} from '@mui/material';
+import { useTheme } from "@mui/system";
+import {Container} from '@mui/material';
+
+
+function HowItWorks() {
+    return (
+        <></>
+    );
+}
+
+export default HowItWorks;
