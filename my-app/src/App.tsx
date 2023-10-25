16344ca588d76e7e96fd746061fab9b21370a318
kimyejean02 <kimyejean02@gmail.com>
2023-10-24 23:51:00 -0500

    merge main

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -5,7 +5,7 @@ import LoginPage from './Pages/LoginPage';
 import SignUpPage from './Pages/SignUpPage'
 import { Login } from '@mui/icons-material';
 import LandingPage from './Pages/LandingPage';
-import Projects from './Pages/Projects';
+import ProjectsPage from './Pages/ProjectsPage';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 {/* <div style={{margin: 0, padding: 0}}>
@@ -20,7 +20,7 @@ function App() {
           <Route path="/" element={<LoginPage />} />
           <Route path="homepage" element={ <LandingPage />} />
           <Route path="signup" element={<SignUpPage />} />
-          <Route path="projects" element={<Projects />} />
+          <Route path="projects" element={<ProjectsPage />} />
         </Routes>
       </main>
     </BrowserRouter>
