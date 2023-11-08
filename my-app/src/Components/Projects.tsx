5bf01a57d844f65d2d9480eac2a082b099db8969
David Pekar <davidpekar@utexas.edu>
2023-11-08 16:50:50 -0600

    small-changes

--- my-app/src/Components/Projects.tsx
+++ my-app/src/Components/Projects.tsx
@@ -2,6 +2,11 @@ import React, { useState } from 'react';
 import { Typography, Button, Container, TextField  } from '@mui/material';
 import { BrowserRouter, Routes, Route, Link, useParams, useLocation, useNavigate} from 'react-router-dom';
 import Project from './Project';
+import axios from 'axios';
+interface LocationState {
+    username: string;
+}
+
 
 
 import axios from 'axios';
@@ -10,6 +15,24 @@ interface LocationState {
 }
 
 function Projects(props: any){
+
+    // const handleGetUserProject = () => {
+    //     console.log("Login clicked")
+    //     console.log(state.username)
+    //     let username = state.username
+    //     const url = `http://localhost:5000/joinProject/${state.username}/${projectId}`;
+    //     axios.post(url)
+    //     .then(res => {
+    //         console.log(res.data);
+    //         if (res.data.success === true) {
+    //             console.log("Join su")
+    //             const url = `/projects`
+    //         }
+    //     })
+    // }
+
+    const location = useLocation()
+    const state = location.state as LocationState
     const project = props.projects.map((proj: any) => {
         console.log(props.projects)
         return <Project name={proj['name']} projectID={proj['id']} description={proj['description']} usedSet1={0} usedSet2={0} capacity={100}/>
